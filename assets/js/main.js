ae4f7d694cba0857d112d6f7bfa41621a222743c
Andrea <andreamarongiu05@gmail.com>
2021-03-24 13:05:08 +0100

    Next e Prev base

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    index: 0,
+    arrayImg: [
+      'http://1.bp.blogspot.com/-6PYPT11obnA/Tlc9KFGj1HI/AAAAAAAAFUc/5I_IeithKZ8/s400/Latest-Nature.jpg',
+      'http://wallpapers.net/californian-nature/download/400x400.jpg',
+      'https://chile.travel/wp-content/uploads/bfi_thumb/parque-Torres-del-Paine-mq6z5gsqkpg9w9fxr86yz617ye4k6q48e5dvqtpz8g.png'
+    ],
+  },
+  methods: {
+    nextImg: function () {
+      // devo prendere indice e spostarlo a seconda delle immagini
+      this.index++
+      if (this.index == 3) {
+        this.index = 0;
+      }
+    },
+    prevImg: function () {
+      // devo prendere indice e spostarlo a seconda delle immagini
+      this.index--
+      if (this.index == -1) {
+        this.index = 2;
+      }
+    }
+  }
+});
